0032ad5311d13ce1419d7b3578bc3451dc231395
TrueTony <spanchbob@gmail.com>
2023-03-01 13:24:01 +0300

    little optz

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('teachbase.urls')),
+    path('teachbase/', include('teachbase.urls')),
 ]
